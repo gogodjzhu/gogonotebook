920c74007f21930428a6080f27e39d8f80df8a4f
gogodjzhu <gogodjzhu@gmail.com>
2020-01-14 00:17:39 +0800

    reset TODO

--- algorithm/abstract/backtracking/generate_parentheses.go
+++ algorithm/abstract/backtracking/generate_parentheses.go
@@ -4,7 +4,7 @@ type GenerateParenthesesSolver struct {
 	res []string
 }
 
-// 动态规划解括号生成器，很容易想到的是使用数组来保存每个括号序列的左括号数量，关键在于如何根据左括号的数量来判断新的括号生成策略
 func (gps *GenerateParenthesesSolver) GenerateParenthesesSolve(n int) []string {
+	// TODO
 	return nil
 }
